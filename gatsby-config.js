a31f0391d76e9dab0a022a89b6900c4817456587
Jorge <djsmoothzouk@gmail.com>
2023-08-09 14:08:38 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Lenz Automtive`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
